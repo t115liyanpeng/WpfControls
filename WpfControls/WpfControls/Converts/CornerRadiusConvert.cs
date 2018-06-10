7f8d2c2fbe5b438e19b7dc666e48bd10563e8785
liyanpeng <t115liyanpeng@126.com>
2018-06-10 17:48:41 +0800

    添加开关按钮

---
+++ WpfControls/WpfControls/Converts/CornerRadiusConvert.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfControls.Converts
+{
+    /// <summary>
+    /// 根据输入的长度来确定圆角的大小
+    /// </summary>
+    public class CornerRadiusConvert:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return 0;
+            }
+            double height = (double) value;
+            if (height > 0)
+            {
+                return height / 2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
